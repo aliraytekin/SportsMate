c643e5b66633fb91847c2a68a0315a03f4f37313
Ali Aytekin <aliraytekin@gmail.com>
2025-07-14 20:27:58 +0300

    Mapbox fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_07_13_181513) do
+ActiveRecord::Schema[7.1].define(version: 2025_07_13_180707) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,11 +57,11 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_13_181513) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "free", default: true
+    t.float "latitude"
+    t.float "longitude"
     t.string "street"
     t.string "city"
     t.string "country"
-    t.float "latitude"
-    t.float "longitude"
     t.index ["sport_id"], name: "index_events_on_sport_id"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
