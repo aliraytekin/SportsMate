c71a228d13e29f7bf94500116f944c77dede0aa0
Alona Shautdinova <alenahtc25@gmail.com>
2025-07-18 21:13:08 +0200

    event show page

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -13,19 +13,31 @@ class ParticipationsController < ApplicationController
   end
 
   def create
-    @participation = Participation.new(participation_params)
     @event = Event.find(params[:event_id])
-    @participation.event = @event
-    @participation.user = current_user
+    authorize @event, :show?
 
+    if @event.participations.exists?(user_id: current_user.id)
+      redirect_to @event, alert: "You're already in this event." and return
+    end
+
+    if @event.participations.count >= @event.max_participants
+      redirect_to @event, alert: "This event is full." and return
+    end
+
+    if @event.user == current_user
+      redirect_to @event, alert: "You cannot join your own event." and return
+    end
+
+    @participation = Participation.new(event: @event, user: current_user)
     authorize @participation
 
     if @participation.save
-      redirect_to event_participations_path(@participation), notice: "You have successfully joined the event."
+      redirect_to @event, notice: "You have successfully joined the event."
     else
-      render :new, status: :unprocessable_entity
+      redirect_to @event, alert: "Failed to join the event."
     end
   end
+
 
   def edit
     @participation = Participation.find(params[:id])
